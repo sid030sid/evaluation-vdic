0917c2154aab762a9b196c646963ea0a492a13ca
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-05-03 14:30:43 +0200

    testing read performance of moralis pinning service done

--- test-suite-pinning-service/moralis-read-test.js
+++ test-suite-pinning-service/moralis-read-test.js
@@ -6,33 +6,26 @@ const path = require('path');
 const data = fs.readFileSync('./moralis-write-performance-measurements.csv', 'utf8').split("\n");
 data.forEach(async line => {
     const lineData = line.split(",");
-    const start = lineData[0];
-    const pinningService = lineData[1];
-    const action = lineData[2];
     const ipfsPath = lineData[3];
     const size = lineData[4];
-    const performance = lineData[5];
 
-    // read from moralis 30 times and measure each time performance
-    for (let i = 0; i < 30; i++) {
-        const start = new Date(); //start timer
-        const readData = moralisPinningService.readFromMoralis(ipfsPath); //read file from moralis
-        const end = new Date(); //end timer
+    // read all 150 files which are 30 versions of the same 5 files in test-data
+    
+    const start = new Date(); //start timer
+    const readData = moralisPinningService.readFromMoralis(ipfsPath); //read file from moralis
+    const end = new Date(); //end timer
+    
 
-        //check if file was read from moralis
-        if(readData === "ERROR"){
-            console.log("Error reading from moralis");
-        }else{
-            //write uploadedFileIPFSpath, writePerformance and size to csv file pinning-service-performance.csv
-            const readPerformance = end - start;
-            const readRes = fs.appendFileSync('./moralis-read-performance-measurements.csv', `${start},${pinningService},read,${ipfsPath},${size},${readPerformance}\n`); 
-            console.log("File successfully read from: ", ipfsPath);
-        }
-
-        //wait 5 seconds till next for loop iteration
-        await new Promise(resolve => setTimeout(resolve, 5000));
+    //check if file was read from moralis
+    if(readData === "ERROR"){
+        console.log("Error reading from moralis");
+    }else{   
+        //write uploadedFileIPFSpath, writePerformance and size to csv file pinning-service-performance.csv
+        const readPerformance = end - start;
+        const readRes = fs.appendFileSync('./moralis-read-performance-measurements.csv', `${start},moralis,read,${ipfsPath},${size},${readPerformance}\n`); 
+        console.log("File successfully read from: ", ipfsPath);
     }
 
-    //wait 10 seconds till next for loop iteration
-    await new Promise(resolve => setTimeout(resolve, 10000));
+    //wait 5 seconds till next for loop iteration
+    await new Promise(resolve => setTimeout(resolve, 5000));
 });
