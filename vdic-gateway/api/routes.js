12c1a866da4b7004981f94d47b5de0a0ae9c27c0
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-05-12 16:37:02 +0200

    writing data and recording write performance works but not reading

--- vdic-gateway/api/routes.js
+++ vdic-gateway/api/routes.js
@@ -3,18 +3,24 @@ const axios = require('axios')
 const fs = require('fs');
 require('dotenv').config()
 
-// HELPER FUNCTIONS
-// function to add a line to a CSV file
-const documentMeasurement = (path, line) => {
-    // append the line to the CSV file
-    fs.appendFile(path, (new Date()).toString()+","+line + '\n', (err) => {
-        if (err) {
-            console.error('Error appending to CSV ("'+path+'"):', err);
-        } else {
-            console.log('Line successfully added to: "'+path+'"');
-        }
-    });
-}
+//IPFS CLUSTER CTL COMMANDS are the following ENDPOINTS:
+    //stauts ls command --> GET: http://127.0.0.1:9094/peers
+
+    //write file to VDICs:
+        //1. add <filepath> --> POST: http://127.0.0.1:9094/add?chunker=size-262144&cid-version=0&format=unixfs&hash=sha2-256&hidden=false&layout=&local=false&mode=recursive&name=&no-pin=false&nocopy=false&progress=false&raw-leaves=false&recursive=false&replication-max=0&replication-min=0&shard=false&shard-size=104857600&stream-channels=true&user-allocations=&wrap-with-directory=false
+                                     // or for 10kb.txt: POST: http://127.0.0.1:9094/add?chunker=size-262144&cid-version=0&format=unixfs&hash=sha2-256&hidden=false&layout=&local=false&mode=recursive&name=&no-pin=false&nocopy=false&progress=false&raw-leaves=false&recursive=false&replication-max=0&replication-min=0&shard=false&shard-size=104857600&stream-channels=true&user-allocations=&wrap-with-directory=false
+        //2. pin add <cid> --> POST: http://127.0.0.1:9094/pins/ipfs/QmP5m4vxj9uUwrEJG53bPw3X2onwoywqevrGZ2Yzfngoay?mode=recursive&name=&replication-max=0&replication-min=0&shard-size=0&user-allocations=
+    
+    //get information about latency
+        //pin ls <cid> --> GET: http://127.0.0.1:9094/allocations/QmP5m4vxj9uUwrEJG53bPw3X2onwoywqevrGZ2Yzfngoay
+                        //do not use: status <cid> --> GET: http://127.0.0.1:9094/pins/QmP5m4vxj9uUwrEJG53bPw3X2onwoywqevrGZ2Yzfngoay?local=false --> here we can get the latency from as timestamp of each node's pin info indicates when the file was added
+        //see difference: https://ipfscluster.io/documentation/reference/ctl/
+
+    //read file from VDICs via IPFS:
+        //1. GET: https://ipfs.io/ipfs/QmP5m4vxj9uUwrEJG53bPw3X2onwoywqevrGZ2Yzfngoay
+
+    // CID of 1kb.txt: QmP5m4vxj9uUwrEJG53bPw3X2onwoywqevrGZ2Yzfngoay
+
 
 // ENDPOINTS FOR AUTHENTICATION
 // TODO: simplfy do not do as in real-life implementation ... here the OID4VP based authentication is performed
@@ -42,26 +48,16 @@ router.route('/read/:cid').get(async (req, res) => {
         // get data from body
         const cid = req.params.cid
 
-        // start measurement
-        const start = new Date()
-
-        // get file from DIC
-        const file = "todo"
-        //TODO
-
-        // end measurement
-        const end = new Date()
+        // get file from VDIC
+        const file = await axios.get(`https://ipfs.io/ipfs/${cid}`)
 
         // send file to user
         if(file){
 
-            // store measurement with scheme: operationType (= read or write), cid, fileSize, executionTime
-                //const measurement ="didCheqdCredentialServiceAPI,tracing,"+didId+","+history.length+","+numberCompartments+","+(end.getTime()-start.getTime())
-                //documentMeasurement("../measurements/tracingMeasurements.csv", measurement)
-
             // send info to requester
             res.send({
-                cid:cid
+                cid:cid,
+                file:file
             })
         }else{
             res.send(`ERROR: reading file ${cid} failed!`)
@@ -71,36 +67,40 @@ router.route('/read/:cid').get(async (req, res) => {
     }
 })
 
-// POST: file to DIC
+// POST: file to VDIC
 router.route('/write').post(async (req, res) => {
     try {
 
         // start measurement
-        const start = new Date()
+        const start = new Date() //TODO: move this to testGateway.js
 
-        // get file from body
-        const file = req.body.file
+        // get path from body
+        const path = req.body.path
 
-        // post file to DIC and get its cid
-        //TODO
-        const cid = "todo"
-
-        // end measurement
-        const end = new Date()
+        // post file to VDIC and get its cid
+        const resWriteToVdic = await axios.post(
+            "http://127.0.0.1:9094/add?chunker=size-262144&cid-version=0&format=unixfs&hash=sha2-256&hidden=false&layout=&local=false&mode=recursive&name=&no-pin=false&nocopy=false&progress=false&raw-leaves=false&recursive=false&replication-max=0&replication-min=0&shard=false&shard-size=104857600&stream-channels=true&user-allocations=&wrap-with-directory=false",
+            {path:path}, //TODO this probably needs to be corrected,
+            {
+                headers: {
+                  'Content-Type': 'multipart/form-data'
+                }
+            }
+        ) // add file                
+        const cid = resWriteToVdic.data.cid
+        
+        const resPinToVdic = await axios.post(
+            "http://127.0.0.1:9094/pins/ipfs/"+cid+"?mode=recursive&name=&replication-max=0&replication-min=0&shard-size=0&user-allocations="
+        ) //pin file
 
         // send file to user
         if(cid){
-
-            // store measurement with scheme: operationType (= read or write), cid, fileSize, executionTime
-                //const measurement ="didCheqdCredentialServiceAPI,tracing,"+didId+","+history.length+","+numberCompartments+","+(end.getTime()-start.getTime())
-                //documentMeasurement("../measurements/tracingMeasurements.csv", measurement)
-
             // send info to requester
             res.send({
-                file:file
+                cid:cid
             })
         }else{
-            res.send(`ERROR: writing file to DIC failed!`)
+            res.send(`ERROR`)
         }
     } catch (error) {
         error.response ? console.error(error.response.data) : console.error(error)
--- vdic-gateway/testGateway.js
+++ vdic-gateway/testGateway.js
@@ -1,4 +1,7 @@
 const axios = require("axios")
+const fs = require('fs');
+const path = require('path');
+const testFolder = '../test-data/';
 
 const secret = "password"
 
@@ -8,40 +11,84 @@ const toBeUploadedJson = {
     "test": "ok?!"
 }
 
+const wait5Seconds = async () => {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+}
+
 const testUploadAndDownloadToVdic = () => {
 
-    // write file from DIC
-    axios.post(
-        "http://localhost:3001/gateway/write", 
-        {
-            file : toBeUploadedJson,
-        },
-        {
-            headers: {
-                'Authorization': 'Basic ' + secret
-            }
-        }
-    )
-    .then(res => {
-        console.log(res.data)
-    }).catch(err => {
-        console.log(err)
-    })
+    // get files from test-data folder
+    fs.readdirSync(testFolder).forEach(async file => {
+        const data = fs.readFileSync(path.join(testFolder, file), 'utf8'); //read to be uploaded files
 
-    // read file from DIC
-    axios.get(
-        "http://localhost:3001/gateway/read/123456",
-        {
-            headers: {
-                'Authorization': 'Basic ' + secret
-            }
+        // upload to moralis 30 times and measure each time performance
+        for (let i = 0; i < 1; i++) { //TOPO: change to 30
+            const start = new Date(); //start timer
+            // write file to VDIC
+            axios.post(
+                "http://localhost:3001/gateway/write", 
+                {
+                    path : path.join(testFolder, file),
+                },
+                {
+                    headers: {
+                        'Authorization': 'Basic ' + secret
+                    }
+                }
+            )
+            .then(res => {
+                const end = new Date(); //end timer
+                const cid = res.data.cid
+
+                //check if file was written to VDICs
+                if(cid === "ERROR"){	
+                    console.log("Error writing to VDIC");
+                }else{
+                    //document write performance
+                    const writePerformance = end - start;
+                    const size = Buffer.byteLength(data, 'utf8') / 1024;
+                    const writeRes = fs.appendFileSync('./vdic-write-performance-measurements.csv', `${start},VDIC,write,${cid},${size},${writePerformance}\n`); 
+
+                    wait5Seconds();
+
+                    // read file from VDIC
+                    const startRead = new Date(); //start timer
+                    axios.get(
+                        "http://localhost:3001/gateway/read/"+cid,
+                        {
+                            headers: {
+                                'Authorization': 'Basic ' + secret
+                            }
+                        }
+                    )
+                    .then(res => {
+                        const endRead = new Date(); //end timer
+                        //check if file was written to VDICs
+                        if(res.data.file){	
+                            //document read performance
+                            const readPerformance = endRead - startRead;
+                            const size = Buffer.byteLength(data, 'utf8') / 1024;
+                            const readRes = fs.appendFileSync('./vdic-read-performance-measurements.csv', `${startRead},VDIC,write,${cid},${size},${readPerformance}\n`);
+                        }else{
+                            console.log("Error reading file with CID: "+cid);
+                        }
+                    }).catch(err => {
+                        console.log(err)
+                    })
+
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+            
+
+            //wait 5 seconds till next for loop iteration
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
-    )
-    .then(res => {
-        console.log(res.data)
-    }).catch(err => {
-        console.log(err)
-    })
+
+        //wait 10 seconds till next for loop iteration
+        await new Promise(resolve => setTimeout(resolve, 10000));
+    });
 }
 
 testUploadAndDownloadToVdic()
